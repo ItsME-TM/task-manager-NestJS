e237d0812a97424f3767bf4380e60f7a4603b873
ItsME-TM <thushanmalaka123@gmail.com>
2025-05-25 21:14:02 +0530

    Update API_URL to use production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://task-manager-nestjs-555l.onrender.com';
 const api = axios.create({
     baseURL: API_URL,
     headers:{
